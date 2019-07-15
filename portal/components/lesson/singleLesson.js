77c8b0d251b6c229917daa9fc37dc55799b4aea8
Sigalit <siga.m.sm@gmail.com>
2019-07-15 19:29:02 +0300

    Add content  to single lesson + status change

--- portal/components/lesson/singleLesson.js
+++ portal/components/lesson/singleLesson.js
@@ -1,6 +1,84 @@
 apple.controller('singleLesson', ['$rootScope', '$scope', '$state', '$stateParams', '$http', '$q', 'userService', 'Upload','server',
     function($rootScope, $scope, $state, $stateParams, $http, $q, userService, Upload, server) {
 
-       
+        $scope.lessonId = $stateParams.lessonId;
+        $scope.lessonNum = $stateParams.lessonNum;
+        $scope.courseId = $stateParams.courseId;
+                           
+        // To get student attendance in a single lesson :
+        // Function name: GetStudentsAttendance
+        // Input: lessonid
+        $scope.getAttendanceOfLesson = function() {
+            var data = {};
+            data.lessonid = $scope.lessonId;
+            server.requestPhp(data, "GetStudentsAttendance").then(function(data) {
+                $scope.stundentsAttandance = data;
+                console.log($scope.stundentsAttandance);
+            });
+        }
+    
+           
+        $scope.getAttendanceOfLesson();
+    
+        // To get attendance option for dropdown:
+        // Function name: GetAttendanceStatuses
+        // Input: -
+        $scope.GetAttendanceStatuses = function() {
+            var data = {};
+            server.requestPhp(data, "GetAttendanceStatuses").then(function(data) {
+                $scope.AttendanceStatusesTags = data;
+                console.log($scope.AttendanceStatusesTags);
+            });
+        }
+    
+        $scope.GetAttendanceStatuses();
+      
+    
+        // To update the status of a single student:
+        // If the student.checkstudentid==null
+        // 	Use:
+        // 	Function name: AddCheckStudentStatus
+        // 	Input: student, lessonid
+        // Else use: 
+    
+        // 	Function name: UpdateCheckStudentStatus
+        // 	Input: student, lessoned
+        
+        $scope.UpdateAttendeeStatus = function(student) {
+            var data={};
+            var async = $q.defer(); 
+            if (student.checkstudentid==null) {
+                data.lessonid=$scope.lessonId;
+                data.status = student.attendanceStatus;
+                data.student = student.userid;
+                console.log(data);
+                server.requestPhp(data, 'AddCheckStudentStatus').then(function (data) {
+                    console.log("Success in saving status");
+                    async.resolve(data); // --> no data came back
+                }, function (err) {
+                    console.error(err);
+                    async.reject(error);
+                });
+                return async.promise;
+            } else {
+                data.lessonid=$scope.lessonId;
+                data.status = student.attendanceStatus;
+                data.student = student.userid;
+                console.log(data);
+                server.requestPhp(data, 'UpdateCheckStudentStatus').then(function (data) {
+                    console.log("Success in saving status");
+                    async.resolve(data); // --> no data came back
+                }, function (err) {
+                    console.error(err);
+                    async.reject(error);
+                });
+                return async.promise;
+            }
+            
+        }
+    
+        $scope.backSingleCourse = function(){
+            $state.transitionTo('singleCourse', {courseId: $scope.courseId});
+        };
 
     }]);
